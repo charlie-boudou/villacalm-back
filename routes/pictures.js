10b4aa6af4838659c6c9f6badd9a7b609bb995e0
charlieboudou <charlie.lcb3@gmail.com>
2024-07-19 10:11:55 +0400

    fix

--- routes/pictures.js
+++ routes/pictures.js
@@ -32,16 +32,18 @@ router.get('/all', async (req, res) => {
 
 /* To get all mockups */
 router.get('/mockup', async (req, res) => {
+  console.log('requete');
   try {
     const result = await cloudinary.search
       .expression('folder:villa-calm/maquettes')
       .execute();
-      
+    console.log('try', result);
     res.status(200).json({
       result: true,
       datas: result.resources,
     });
   } catch (error) {
+    console.log('catch', error);
     return res.json({
       result: false,
       error: error,
